2a92c510894b28e4bf308e4e5dc20c6dd4035d98
BCrawf521 <166097@student.asd20.org>
2018-11-14 14:51:34 -0700

    I made a Stream Writer and a Stream Reader that writes and pulls data from a file.

---
+++ Archive 11-2-18/Visual Studio/Stream Reader/Stream Reader/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream_Reading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            int[,] a = new int[4, 4];
+            List<string> b = new List<string>();
+
+            string path = @"C:\Users\166097\Documents\Junior-Year-Coding-Class\Archive 11-2-18\Visual Studio\Stream Written File\Example.txt";
+            path = AppDomain.CurrentDomain.BaseDirectory + @"Example.txt";
+            string uinput;
+
+            do
+            {
+                Console.WriteLine("Press a number based on which action you would like to perform:");
+                Console.WriteLine("1. Save a random array to a file");
+                Console.WriteLine("2. Load a random array from a file to List b");
+                Console.WriteLine("3. Print List b");
+                uinput = Console.ReadLine();
+
+                if (uinput == "1")
+                {
+                    using (StreamWriter sw = new StreamWriter(path))
+                    {
+                        for (int i = 0; i < a.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < a.GetLength(0); j++)
+                            {
+                                a[i, j] = rand.Next(0, 10);
+                            }
+                        }
+
+                        for (int i = 0; i < a.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < a.GetLength(0); j++)
+                            {
+                                sw.Write(a[i, j]);
+                            }
+                            sw.WriteLine();
+                        }
+                        sw.Dispose();
+                    }
+                }
+
+                if (uinput == "2")
+                {
+                    b.Clear();
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            b.Add(line);
+                        }
+                    }
+                }
+
+                if (uinput == "3")
+                {
+                    for (int i = 0; i < b.Count; i++)
+                    {
+                        Console.Write(b[i] + "");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            while (true);
+        }
+    }
+}
---
+++ Archive 11-2-18/Visual Studio/Stream Writing/Stream Writing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream_Writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand = new Random();
+            int[] sArray = new int[10];
+            int[,] s2DArray = new int[4, 4];
+
+            string path = @"C:\Users\166097\Documents\Junior-Year-Coding-Class\Archive 11-2-18\Visual Studio\Stream Written File\Example.txt";
+            path = AppDomain.CurrentDomain.BaseDirectory + @"Example.txt";
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+
+                for (int i = 0; i < sArray.Length; i++)
+                {
+                    sArray[i] = rand.Next(0, 10);
+                }
+
+                for (int i = 0; i < sArray.Length; i++)
+                {
+                    sw.Write(sArray[i] + "");
+                }
+                sw.WriteLine();
+                sw.WriteLine();
+
+                for (int i = 0; i < s2DArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < s2DArray.GetLength(0); j++)
+                    {
+                        s2DArray[i, j] = rand.Next(0, 10);
+                    }
+                }
+
+                for (int i = 0; i < s2DArray.GetLength(0); i++)
+                {
+                    for (int j = 0; j < s2DArray.GetLength(0); j++)
+                    {
+                        sw.Write(s2DArray[i, j]);
+                    }
+                    sw.WriteLine();
+                }
+                sw.Dispose();
+            }
+        }
+    }
+}
