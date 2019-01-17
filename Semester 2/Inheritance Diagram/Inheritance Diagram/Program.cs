32adfbd0f7105f39dcc9f02f4eb7bdb58d7e5214
BCrawf521 <166097@student.asd20.org>
2019-01-17 12:11:08 -0700

    I am NOW finished with this project.

--- Semester 2/Inheritance Diagram/Inheritance Diagram/Program.cs
+++ Semester 2/Inheritance Diagram/Inheritance Diagram/Program.cs
@@ -11,20 +11,22 @@ namespace Inheritance_Diagram
         static void Main(string[] args)
         {
             Console.WriteLine("Press any key to see all of my vehicles!");
+            Console.WriteLine();
             Console.ReadKey();
 
             List<Automobile> vehicles = new List<Automobile>();
-            vehicles.Add(new Car(120, 27, "gray", false, true, 7, true));
-            vehicles.Add(new Truck(130, 32, "blue", true, true, 8, 11700, true));
-            vehicles.Add(new Motorcycle(140, 22, "orange", false, false, 2.9f, true));
-            vehicles.Add(new SportsCar(180, 25, "yellow", false, false, 5, false, true));
-            vehicles.Add(new SUV(130, 35, "white", false, true, 8, true, 33.5f));
-            vehicles.Add(new DirtBike(150, 23, "green", true, false, 2.7f, false, 0.4f));
-            vehicles.Add(new SportsMotorcycle(190, 34, "purple", false, false, 2.5f, true, true));
-            vehicles.Add(new TouringMotorcycle(160, 39, "black", false, true, 3f, true, 30));
+            vehicles.Add(new Car(120, 27, "Gray", false, true, 7, true));
+            vehicles.Add(new Truck(130, 32, "Blue", true, true, 8, 11700, true));
+            vehicles.Add(new Motorcycle(140, 22, "Orange", false, false, 2.9f, true));
+            vehicles.Add(new SportsCar(180, 25, "Yellow", false, false, 5, false, true));
+            vehicles.Add(new SUV(130, 35, "White", false, true, 8, true, 33.5f));
+            vehicles.Add(new DirtBike(150, 23, "Green", true, false, 2.7f, false, 0.4f));
+            vehicles.Add(new SportsMotorcycle(190, 34, "Purple", false, false, 2.5f, true, true));
+            vehicles.Add(new TouringMotorcycle(160, 39, "Black", false, true, 3f, true, 30));
 
             for (int i = 0; i < vehicles.Count; i++)
             {
+                vehicles[i].PrintVehicle();
                 vehicles[i].Honk();
                 vehicles[i].GoVroom();
                 Console.WriteLine();
