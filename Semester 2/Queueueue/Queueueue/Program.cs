00093e773972955ae6f975e12542ae5f6a1abe66
BCrawf521 <166097@student.asd20.org>
2019-01-28 13:41:35 -0700

    Finished a Queue class that manipulates a queue.

---
+++ Semester 2/Queueueue/Queueueue/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queueueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue myQueue = new Queue();
+            int input;
+
+            Console.Write("Welcome to my queue! ");
+
+            do
+            {
+                Menu:
+                Console.WriteLine("Press a number based on the action that you want to perform(-1 to quit).");
+                Console.WriteLine("1. Add a number to the queue");
+                Console.WriteLine("2. View the first element of the queue *without* deleting it");
+                Console.WriteLine("3. View the first element of the queue and delete it");
+                Console.WriteLine("4. Print the whole queue");
+                Console.WriteLine("5. Clear the queue");
+                Console.WriteLine();
+
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.WriteLine("Enter a number to add to the queue.");
+                    int input2 = int.Parse(Console.ReadLine());
+
+                    myQueue.EnQueue(input2);
+
+                    Console.WriteLine("Number added.");
+                    Console.WriteLine();
+                }
+
+                if (input == 2)
+                {
+                    int temp = myQueue.Peek();
+
+                    if (temp == Int32.MaxValue)
+                    {
+                        Console.WriteLine("Oops! You tried to peek from an empty queue!");
+                        Console.WriteLine();
+                        goto Menu;
+                    }
+
+                    Console.WriteLine("The first element of this queue is: " + temp);
+                    Console.WriteLine();
+                }
+
+                if (input == 3)
+                {
+                    int temp = myQueue.DeQueue();
+
+                    if (temp == Int32.MaxValue)
+                    {
+                        Console.WriteLine("Oops! You tried to remove a number from an empty queue!");
+                        Console.WriteLine();
+                        goto Menu;
+                    }
+
+                    Console.WriteLine("The first element of this queue is: " + temp);
+                    Console.WriteLine("This number was also removed from the queue.");
+                    Console.WriteLine();
+                }
+
+                if (input == 4)
+                {
+                    Console.WriteLine("Here is your queue:");
+                    myQueue.Print();
+                    Console.WriteLine();
+                }
+
+                if (input == 5)
+                {
+                    Console.WriteLine("Your queue was cleared!");
+                    myQueue.Clear();
+                    myQueue.Print();
+                    Console.WriteLine();
+                }
+
+            } while (input != -1);
+        }
+    }
+}
---
+++ Semester 2/Queueueue/Queueueue/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Queueueue
+{
+    class Queue
+    {
+        List<int> queue = new List<int>();
+
+        public void EnQueue(int val)
+        {
+            queue.Insert(0, val);
+        }
+
+        public int Peek()
+        {
+            if (queue.Count == 0)
+            {
+                return Int32.MaxValue;
+            }
+
+            return queue[queue.Count - 1];
+        }
+
+        public int DeQueue()
+        {
+            if (queue.Count == 0)
+            {
+                return Int32.MaxValue;
+            }
+
+            int hold = queue[queue.Count - 1];
+            queue.RemoveAt(queue.Count - 1);
+            return hold;
+        }
+
+        public void Print()
+        {
+            Console.Write("--------------------");
+            Console.WriteLine();
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                Console.Write(queue[i] + " ");
+
+                if (i == queue.Count - 1)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Empty queue!");
+            }
+
+            Console.Write("--------------------");
+            Console.WriteLine();
+        }
+
+        public void Clear()
+        {
+            queue.Clear();
+        }
+    }
+}
