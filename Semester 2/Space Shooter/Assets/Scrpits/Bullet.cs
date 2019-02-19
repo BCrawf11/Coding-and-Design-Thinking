4ead1cc6599c96175a3e17f53a490fd05ca94ebc
BCrawf521 <166097@student.asd20.org>
2019-02-19 14:31:31 -0700

    Started a Space Shooter game with unity for a prototype project.

---
+++ Semester 2/Space Shooter/Assets/Scrpits/Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    float timer = 0;
+    float timeToAction = 5;
+    public Vector3 velocity = new Vector3(0, 0, 0);
+    public GameObject BulletPrefab;
+    float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+    }
+
+    /// <summary>
+    /// Initialize the ball
+    /// </summary>
+    /// <param name="position">Position to spawn</param>
+    /// <param name="velocity">Direction * speed</param>
+    /// <param name="color">Color of the ball</param>
+    /// 
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeToAction)
+        {
+            DestroyImmediate(gameObject);
+            timer = 0;
+        }
+    }
+
+    public void Initialize(Vector3 position, Vector3 velocity)
+    {
+        this.transform.position = position;
+        this.velocity = velocity;
+        GetComponent<Rigidbody2D>().velocity = velocity * speed;
+    }
+}
+
---
+++ Semester 2/Space Shooter/Assets/Scrpits/Ship.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ship : MonoBehaviour {
+
+    List<GameObject> bullets = new List<GameObject>();
+    float speed = 7f;
+    Vector3 velocity;
+    Rigidbody2D rbody;
+    public GameObject BulletPrefab;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        velocity = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity += Direction(transform.eulerAngles.z + 90);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity += Direction(transform.eulerAngles.z - 90);
+        }
+
+        rbody.velocity = velocity * (speed);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ShootBalls();
+        }
+    }
+
+    public Vector3 Direction(float eulerAnglesZ)
+    {
+        return new Vector3(Mathf.Cos(eulerAnglesZ * Mathf.Deg2Rad), Mathf.Sin(eulerAnglesZ * Mathf.Deg2Rad), 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Bullet")
+        {
+            Destroy(collision.collider.gameObject);
+            SceneManager.LoadScene("Game Over");
+        }
+    }
+
+    void ShootBalls()
+    {
+        GameObject newBullet = Instantiate(BulletPrefab);
+        newBullet.GetComponent<Bullet>().Initialize(transform.position + .3f * (Direction(transform.eulerAngles.z - 90).normalized),
+            Direction(transform.eulerAngles.z - 90));
+
+        bullets.Add(newBullet);
+    }
+}
