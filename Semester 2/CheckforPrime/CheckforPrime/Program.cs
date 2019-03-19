0162c6b8854ccc9cb4b3c987e3b12773a42cb79f
BCrawf521 <166097@student.asd20.org>
2019-03-19 14:49:15 -0600

    Recursion!! (And stringbits)

---
+++ Semester 2/CheckforPrime/CheckforPrime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckforPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckForPrime(27427));
+        }
+
+        static bool CheckForPrime(int n, int divisor = 2)
+        {
+            if (n % divisor == 0)
+                return false;
+
+            if (divisor == n - 1)
+                return true;
+
+            return CheckForPrime(n, divisor + 1);
+        }
+    }
+}
---
+++ Semester 2/StringBits/StringBits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ex = "hello";
+            string newstring = "";
+
+            for (int i = 0; i < ex.Length; i += 2)
+            {
+                newstring += ex[i];
+            }
+
+            Console.WriteLine(newstring);
+        }
+    }
+}
