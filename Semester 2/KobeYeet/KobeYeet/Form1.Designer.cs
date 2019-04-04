a61e7587ac58019ac0aabc0fa226fb1764ac0d66
BCrawf521 <166097@student.asd20.org>
2019-04-04 14:51:43 -0600

    Did some stuff with Winforms :)

---
+++ Semester 2/KobeYeet/KobeYeet/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace KobeYeet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_Kobe_Yeet = new System.Windows.Forms.Button();
+            this.CheckBox_FailSafe = new System.Windows.Forms.CheckBox();
+            this.KobeDate = new System.Windows.Forms.DateTimePicker();
+            this.Label_Yeet = new System.Windows.Forms.Label();
+            this.Picture_Box = new System.Windows.Forms.PictureBox();
+            this.Its_Free_Text = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button_Kobe_Yeet
+            // 
+            this.Button_Kobe_Yeet.Location = new System.Drawing.Point(81, 66);
+            this.Button_Kobe_Yeet.Name = "Button_Kobe_Yeet";
+            this.Button_Kobe_Yeet.Size = new System.Drawing.Size(349, 300);
+            this.Button_Kobe_Yeet.TabIndex = 0;
+            this.Button_Kobe_Yeet.Text = "KOBEYEET";
+            this.Button_Kobe_Yeet.UseVisualStyleBackColor = true;
+            this.Button_Kobe_Yeet.Click += new System.EventHandler(this.Button_Kobe_Yeet_Click);
+            // 
+            // CheckBox_FailSafe
+            // 
+            this.CheckBox_FailSafe.AutoSize = true;
+            this.CheckBox_FailSafe.Location = new System.Drawing.Point(81, 369);
+            this.CheckBox_FailSafe.Name = "CheckBox_FailSafe";
+            this.CheckBox_FailSafe.Size = new System.Drawing.Size(75, 17);
+            this.CheckBox_FailSafe.TabIndex = 1;
+            this.CheckBox_FailSafe.Text = "FAiL SaFe";
+            this.CheckBox_FailSafe.UseVisualStyleBackColor = true;
+            this.CheckBox_FailSafe.CheckedChanged += new System.EventHandler(this.CheckBox_FailSafe_CheckedChanged);
+            // 
+            // KobeDate
+            // 
+            this.KobeDate.Location = new System.Drawing.Point(89, 40);
+            this.KobeDate.Name = "KobeDate";
+            this.KobeDate.Size = new System.Drawing.Size(195, 20);
+            this.KobeDate.TabIndex = 2;
+            this.KobeDate.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Label_Yeet
+            // 
+            this.Label_Yeet.AutoSize = true;
+            this.Label_Yeet.Location = new System.Drawing.Point(229, 369);
+            this.Label_Yeet.Name = "Label_Yeet";
+            this.Label_Yeet.Size = new System.Drawing.Size(55, 13);
+            this.Label_Yeet.TabIndex = 3;
+            this.Label_Yeet.Text = "LabelYeet";
+            // 
+            // Picture_Box
+            // 
+            this.Picture_Box.Location = new System.Drawing.Point(436, 66);
+            this.Picture_Box.Name = "Picture_Box";
+            this.Picture_Box.Size = new System.Drawing.Size(265, 300);
+            this.Picture_Box.TabIndex = 4;
+            this.Picture_Box.TabStop = false;
+            // 
+            // Its_Free_Text
+            // 
+            this.Its_Free_Text.Location = new System.Drawing.Point(290, 40);
+            this.Its_Free_Text.Name = "Its_Free_Text";
+            this.Its_Free_Text.Size = new System.Drawing.Size(411, 20);
+            this.Its_Free_Text.TabIndex = 5;
+            this.Its_Free_Text.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Its_Free_Text);
+            this.Controls.Add(this.Picture_Box);
+            this.Controls.Add(this.Label_Yeet);
+            this.Controls.Add(this.KobeDate);
+            this.Controls.Add(this.CheckBox_FailSafe);
+            this.Controls.Add(this.Button_Kobe_Yeet);
+            this.Name = "Form1";
+            this.Text = "KobeYeet";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture_Box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_Kobe_Yeet;
+        private System.Windows.Forms.CheckBox CheckBox_FailSafe;
+        private System.Windows.Forms.DateTimePicker KobeDate;
+        private System.Windows.Forms.Label Label_Yeet;
+        private System.Windows.Forms.PictureBox Picture_Box;
+        private System.Windows.Forms.TextBox Its_Free_Text;
+    }
+}
+
---
+++ Semester 2/KobeYeet/KobeYeet/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KobeYeet
+{
+    public partial class Form1 : Form
+    {
+        int seed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Button_Kobe_Yeet.MouseEnter += (o, e) => ChangeColor(Color.Cyan);
+            Button_Kobe_Yeet.MouseLeave += ChangeColor;
+        }
+
+        public void ChangeColor(Color c)
+        {
+            Button_Kobe_Yeet.BackColor = c;
+        }
+
+        public void ChangeColor(object o, EventArgs e)
+        {
+            Button_Kobe_Yeet.BackColor = Color.Green;
+        }
+
+        private void Button_Kobe_Yeet_Click(object sender, EventArgs e)
+        {
+            
+
+            if (CheckBox_FailSafe.Checked && KobeDate.Value < DateTime.Now)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Bitmap b = new Bitmap(Picture_Box.Width, Picture_Box.Height);
+                Random rand = new Random();
+
+                switch (seed)
+                {
+                    case 0:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 10) == 0 ? Color.Green : ((x + y) % 10) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 1;
+                        break;
+
+                    case 1:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 12) == 0 ? Color.Green : ((x + y) % 12) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 2;
+                        break;
+
+                    case 2:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 14) == 0 ? Color.Green : ((x + y) % 14) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 3;
+                        break;
+
+                    case 3:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 16) == 0 ? Color.Green : ((x + y) % 16) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 4;
+                        break;
+
+                    case 4:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 18) == 0 ? Color.Green : ((x + y) % 18) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 5;
+                        break;
+
+                    case 5:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 20) == 0 ? Color.Green : ((x + y) % 20) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 6;
+                        break;
+
+                    case 6:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 22) == 0 ? Color.Green : ((x + y) % 22) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 7;
+                        break;
+
+                    case 7:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 24) == 0 ? Color.Green : ((x + y) % 24) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 8;
+                        break;
+
+                    case 8:
+
+                        for (int x = 0; x < Picture_Box.Width; x += 1)
+                        {
+                            for (int y = 0; y < Picture_Box.Height; y += 1)
+                            {
+                                b.SetPixel(x, y, ((x - y) % 26) == 0 ? Color.Green : ((x + y) % 26) == 0 ? Color.Green : Color.Cyan);
+                            }
+                        }
+
+                        Picture_Box.Image = b;
+                        seed = 0;
+                        break;
+
+                }
+            }
+        }
+
+        private void CheckBox_FailSafe_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Its_Free_Text.Text == "Quit Program")
+            {
+                Thread.Sleep(2000);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
